39e8b574cb218f2adda8fd5e33468fcc20e2f2c1
Paras <parasverma0527@gmail.com>
2021-07-18 22:03:24 +0530

    initial commit

---
+++ Paras Prog/Paras.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main ()
+{
+int a,b;
+printf("enter the first number ");
+scanf("%d",&a);
+printf("enter theinclude second number");
+scanf("%d",&b);
+int c =a+b;
+printf("sum of 2 numbers is ;%d",c);
+
+}
